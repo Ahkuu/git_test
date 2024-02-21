e9dcad8f0f47df7a692a952db2e37355311669e5
Ahkuu <Dylantwhite89@yahoo.com>
2024-02-21 20:50:06 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let name = 'Dylan';
+let firstName = 'That boy';
+
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name) 
+// Cannot contain a space or hyphon (-)
+// Are case sensitive
+
+
+console.log(firstName, name);
+
+let interestRate = 0.3;
+console.log(interestRate);
+
+// How to declare a variable
